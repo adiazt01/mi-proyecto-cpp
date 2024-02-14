edc3b542c57df1a0aebc55ac373adb1ace15e24d
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-02-13 20:57:00 -0400

    Refactor main.cpp: Remove inappropriate comments and add welcome message

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
-   std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
-      std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
-
-    std::cout << "Gay el que lo lea" << std::endl;
-
-    //te crees chistosito hijo de tu puta madre
+    std::cout << "Bienvenido al proyecto 😎" << std::endl;
 
     return 0;
 }
