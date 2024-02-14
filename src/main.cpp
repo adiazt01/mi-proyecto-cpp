dd6d076cf51ee07997864bc3b5302e642a312581
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-02-13 20:27:03 -0400

    ups

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
    std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
       std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
 
+    std::cout << "Gay el que lo lea" << std::endl;
 
     return 0;
 }
