0252810ff984f11089f34cb606a468acc15f30b9
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-02-13 20:56:16 -0400

    Update main.cpp with new output messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Bienvenido a nuestro proyecto! :D" << std::endl;
+int main() {
+    std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
+   std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
+      std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
+
+    std::cout << "Gay el que lo lea" << std::endl;
+
+    //te crees chistosito hijo de tu puta madre
+
     return 0;
 }
