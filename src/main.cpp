ac63dfd6358cc399d976c50dbf006dea0acb686a
AdamasLight <gobertalejandro@gmail.com>
2024-02-13 20:26:47 -0400

    a

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 int main() {
    std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
    std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
+      std::cout << "Armando deja la el lol por favor busca ayuda maldita puta" << std::endl;
+
 
     return 0;
 }
